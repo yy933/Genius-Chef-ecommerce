8862248ff3730a7fc9b322b4745c6b00dc37e7e2
yy933 <emilyyu025@gmail.com>
2023-05-03 17:28:59 +0800

    feat: add navbar and footer

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-const express = require("express");
-const exphbs = require("express-handlebars");
-const methodOverride = require("method-override");
-const bcrypt = require("bcryptjs");
-const app = express();
-const PORT = 3000;
-app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
-app.set("view engine", "hbs");
-app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride("_method"));
-app.get("/", (req, res) => {
-  res.send("hello world");
-});
+const express = require('express')
+const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
+const bcrypt = require('bcryptjs')
+const app = express()
+const PORT = 3000
+app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
 app.listen(PORT, () => {
-  console.log(`App is running on http://localhost:${PORT}`);
-});
+  console.log(`App is running on http://localhost:${PORT}`)
+})
