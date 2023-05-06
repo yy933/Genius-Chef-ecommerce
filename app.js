172f6366dfc31755ea19d401c5bedc3b676bf187
yy933 <emilyyu025@gmail.com>
2023-05-06 18:09:21 +0800

    feat: initialize eslint

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
+const axios = require('axios')
 const session = require('express-session')
 const flash = require('connect-flash')
 const path = require('path')
@@ -33,8 +34,9 @@ app.use((req, res, next) => {
 app.get('/', (req, res) => {
   res.render('index')
 })
-app.get('/menu', (req, res) => {
-  res.render('menu')
+
+app.get('/recipe', (req, res) => {
+  res.render('recipe')
 })
 app.get('/contact', (req, res) => {
   return res.render('contact')
@@ -51,6 +53,22 @@ app.post('/contact', (req, res) => {
     console.log(error)
     req.flash('warning_msg', 'Message could not be sent!')
     return res.redirect('/contact')
+  }
+})
+app.get('/menu', async (req, res) => {
+  try {
+    const response = await axios.get('https://api.spoonacular.com/recipes/random', {
+      params: {
+        apiKey: '03fc4a9e3f1d4b6891a0cb8385ccd640',
+        limitLicense: true,
+        number: 3
+      }
+    })
+    const recipes = response.data.recipes
+    console.log(recipes)
+    res.status(200).json(recipes)
+  } catch (err) {
+    res.status(500).json({ message: err })
   }
 })
 app.listen(PORT, () => {
