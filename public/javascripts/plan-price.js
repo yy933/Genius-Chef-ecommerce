f6aacf66887aca84dc479dd274a9a0db907e7815
yy933 <emilyyu025@gmail.com>
2023-05-13 00:10:47 +0800

    style: formatting code

--- public/javascripts/plan-price.js
+++ public/javascripts/plan-price.js
@@ -7,13 +7,13 @@ function calculateTotal () {
   let singleMealPrice
   const totalMeals = servings.value * meals.value
   if (totalMeals >= 25) {
-    singleMealPrice = 8.99
+    singleMealPrice = 7.99
   } else if (totalMeals >= 17) {
-    singleMealPrice = 9.29
+    singleMealPrice = 8.29
   } else if (totalMeals >= 9) {
-    singleMealPrice = 9.49
+    singleMealPrice = 8.49
   } else {
-    singleMealPrice = 9.99
+    singleMealPrice = 8.99
   }
   const total = (singleMealPrice * totalMeals).toFixed(2)
   totalAmount.textContent = total
