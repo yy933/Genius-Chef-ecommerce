ef0d6b44fad63e9a92e331b322379d8de12edd44
yy933 <emilyyu025@gmail.com>
2023-05-16 18:40:19 +0800

    feat: create order model

---
+++ migrations/20230516103846-create-order.js
@@ -0,0 +1,64 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Orders', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      menu: {
+        type: Sequelize.STRING
+      },
+      preference: {
+        type: Sequelize.TEXT
+      },
+      servings: {
+        type: Sequelize.INTEGER
+      },
+      meals: {
+        type: Sequelize.INTEGER
+      },
+      duration: {
+        type: Sequelize.INTEGER
+      },
+      total_amount: {
+        type: Sequelize.FLOAT
+      },
+      delivery_name: {
+        type: Sequelize.STRING
+      },
+      delivery_email: {
+        type: Sequelize.STRING
+      },
+      delivery_phone: {
+        type: Sequelize.STRING
+      },
+      delivery_address: {
+        type: Sequelize.TEXT
+      },
+      preferred_day: {
+        type: Sequelize.STRING
+      },
+      preferred_time: {
+        type: Sequelize.STRING
+      },
+      status: {
+        type: Sequelize.STRING
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Orders');
+  }
+};
---
+++ models/order.js
@@ -0,0 +1,37 @@
+'use strict'
+const {
+  Model
+} = require('sequelize')
+module.exports = (sequelize, DataTypes) => {
+  class Order extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate (models) {
+      // define association here
+    }
+  }
+  Order.init({
+    menu: DataTypes.STRING,
+    preference: DataTypes.TEXT,
+    servings: DataTypes.INTEGER,
+    meals: DataTypes.INTEGER,
+    duration: DataTypes.INTEGER,
+    total_amount: DataTypes.FLOAT,
+    delivery_name: DataTypes.STRING,
+    delivery_email: DataTypes.STRING,
+    delivery_phone: DataTypes.STRING,
+    delivery_address: DataTypes.TEXT,
+    preferred_day: DataTypes.STRING,
+    preferred_time: DataTypes.STRING,
+    status: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Order',
+    tableName: 'Orders',
+    underscored: true
+  })
+  return Order
+}
