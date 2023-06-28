5bdf6b955dbeeaff1370ac470f72a68cc2006fe8
yy933 <emilyyu025@gmail.com>
2023-06-28 16:37:02 +0800

    feat: update seeders

--- seeders/20230527070248-users-seed-file.js
+++ seeders/20230527070248-users-seed-file.js
@@ -21,7 +21,7 @@ module.exports = {
         updated_at: new Date()
       }], {}),
       queryInterface.bulkInsert('Users',
-        Array.from({ length: 9 }, () => ({
+        Array.from({ length: 31 }, () => ({
           name: faker.person.fullName(),
           email: faker.internet.email(),
           password: bcrypt.hashSync(faker.internet.password({ length: 16 }), bcrypt.genSaltSync(10), null),
