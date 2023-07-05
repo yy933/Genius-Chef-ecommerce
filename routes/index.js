8fe6660ac5dceaf9003fe9ae73821e819ef0af4a
yy933 <emilyyu025@gmail.com>
2023-07-05 18:15:43 +0800

    feat: add page not found route

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,13 @@ const express = require('express')
 const router = express.Router()
 
 const user = require('./modules/user')
-// const admin = require('./modules/admin')
 const auth = require('./modules/auth')
 const menu = require('./modules/menu')
 const contact = require('./modules/contact')
 const order = require('./modules/order')
 const admin = require('./modules/admin')
 const errorHandler = require('../middleware/error-handler')
+
 router.use('/users', user)
 router.use('/auth', auth)
 router.use('/menu', menu)
@@ -25,4 +25,5 @@ router.get('/', (req, res, next) => {
 })
 router.use(errorHandler.errorLogger)
 router.use(errorHandler.errorResponder)
+
 module.exports = router
