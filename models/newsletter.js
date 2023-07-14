51fc2e7cb8f4c8d61335fc09a507bf4e3e3d448b
yy933 <emilyyu025@gmail.com>
2023-07-14 16:53:49 +0800

    feat: add subscribe newsletter feature

--- models/newsletter.js
+++ models/newsletter.js
@@ -3,38 +3,29 @@ const {
   Model
 } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
-  class Order extends Model {
+  class Newsletter extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate (models) {
-      Order.belongsTo(models.User, { foreignKey: 'userId' })
-      Order.hasOne(models.Payment)
-      Order.hasOne(models.Delivery)
+      // define association here
     }
   }
-  Order.init({
+  Newsletter.init({
     id: {
       type: DataTypes.INTEGER,
       primaryKey: true,
       allowNull: false,
       autoIncrement: true
     },
-    menu: DataTypes.STRING,
-    preference: DataTypes.TEXT,
-    servings: DataTypes.INTEGER,
-    meals: DataTypes.INTEGER,
-    totalAmount: DataTypes.FLOAT,
-    status: DataTypes.STRING,
-    userId: DataTypes.INTEGER,
-    showId: DataTypes.STRING
+    email: DataTypes.STRING
   }, {
     sequelize,
-    modelName: 'Order',
-    tableName: 'Orders',
+    modelName: 'Newsletter',
+    tableName: 'Newsletters',
     underscored: true
   })
-  return Order
+  return Newsletter
 }
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
-
+const { Newsletter } = require('../models')
+const mailService = require('../helpers/email-helpers')
 const user = require('./modules/user')
 const auth = require('./modules/auth')
 const menu = require('./modules/menu')
@@ -8,6 +9,7 @@ const contact = require('./modules/contact')
 const order = require('./modules/order')
 const admin = require('./modules/admin')
 const errorHandler = require('../middleware/error-handler')
+const validator = require('email-validator')
 
 router.use('/users', user)
 router.use('/auth', auth)
@@ -15,14 +17,57 @@ router.use('/menu', menu)
 router.use('/contact', contact)
 router.use('/orders', order)
 router.use('/admin', admin)
+
+// non-authenticated routes
 router.get('/plans', (req, res, next) => {
   try {
     return res.render('plans')
   } catch (err) { next(err) }
 })
+
 router.get('/instructions', (req, res, next) => {
   try {
     return res.render('how-it-works')
+  } catch (err) { next(err) }
+})
+
+router.post('/newsletter', async (req, res, next) => {
+  try {
+    const { email } = req.body
+    const errors = []
+    if (!email) {
+      errors.push({ message: 'All fields are required.' })
+    }
+    if (!validator.validate(email)) {
+      errors.push({ message: 'Please provide a valid email.' })
+    }
+    if (errors.length) {
+      return res.redirect('/')
+    }
+    const user = await Newsletter.findOne({ where: { email } })
+    if (user) {
+      errors.push({ message: 'You have already subscribed!' })
+      return res.render('index', {
+        errors,
+        email
+      })
+    }
+    const mailOptions = {
+      from: process.env.EMAIL,
+      to: email,
+      subject: 'Thank you for subscribing Genius Chef newsletter!',
+      html: `<h1 style="color:#196F3D; text-align:center">Welcome!</h1>
+            <h3>Dear ${email},</h3>
+            <p style="font-size: 14px">Thank you for subscribing Genius Chef newsletter. We'll keep you updated with news, offers, recipes, and more! Stay tuned and cook with us!</p>
+            <img src="https://images.unsplash.com/photo-1489743342057-3448cc7c3bb9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=412&q=80">
+            <br>
+            <p style="font-size: 14px; color:#616A6B;">Please <a href="${process.env.BASE_URL}/contact">contact us</a> if you have any questions.</p>
+            <br>
+            <h3>Sincerely,<br>Genius Chef Customer Service Team</h3>`
+    }
+    await Promise.all([Newsletter.create({ email }), mailService(mailOptions)])
+    req.flash('success_msg', 'Successfully created an account!')
+    return res.redirect('/')
   } catch (err) { next(err) }
 })
 router.get('/', (req, res, next) => {
