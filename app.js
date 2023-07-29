6253aaa94f32c351e98a36e0a6ad67638e58abb4
yy933 <emilyyu025@gmail.com>
2023-07-29 15:37:49 +0800

    chore: update remote database connection settings

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-// if (process.env.NODE_ENV !== 'production') {
-//   require('dotenv').config()
-// }
-require('dotenv').config()
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const express = require('express')
 const exphbs = require('express-handlebars')
 require('handlebars-helpers')(exphbs)
