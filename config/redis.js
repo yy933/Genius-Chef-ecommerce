29e720748a9dd38f5ce1750c15deddd762e4d6ba
yy933 <emilyyu025@gmail.com>
2023-08-23 15:35:21 +0800

    docs: update .env.example

--- config/redis.js
+++ config/redis.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const Redis = require('ioredis')
 let redis = new Redis() // Connect to 127.0.0.1:6379
 if (process.env.NODE_ENV === 'production') {
-  redis = new Redis(process.env.REDIS_DB_URL)
+  redis = new Redis(process.env.REDIS_DB_URI)
 }
 
 redis.on('error', (error) => {
