c8a24c90de2d10c8756b33834d688f60ce3ab36d
yy933 <emilyyu025@gmail.com>
2023-08-23 15:13:55 +0800

    style: restyle code

--- config/redis.js
+++ config/redis.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const Redis = require('ioredis')
-let redis = new Redis()
+let redis = new Redis() // Connect to 127.0.0.1:6379
 if (process.env.NODE_ENV === 'production') {
   redis = new Redis(process.env.REDIS_DB_URL)
 }
--- controllers/menu-controller.js
+++ controllers/menu-controller.js
@@ -67,7 +67,7 @@ const menuController = {
           }))
 
           const recipesData = recipesRawData.slice(offset, offset + limit)
-
+          // save recipe data in cache
           redis.set(preference, JSON.stringify(recipesRawData), 'EX', 86400, err => {
             if (err) console.log(err)
           })
@@ -80,48 +80,6 @@ const menuController = {
           })
         }
       })
-
-      // menu api
-      // let tags
-      // const preference = req.params.preference
-      // if (preference === 'classic') {
-      //   tags = ''
-      // } else {
-      //   tags = preference
-      // }
-      // const recipes = await axios({
-      //   method: 'get',
-      //   url: 'https://api.spoonacular.com/recipes/random',
-      //   headers: { 'Content-Type': 'application/json' },
-      //   params: {
-      //     limitLicense: true,
-      //     number: DEFAULT_AMOUNT,
-      //     tags,
-      //     apiKey: process.env.API_KEY
-      //   }
-      // })
-      // // recipes data
-      // const recipesData = recipes.data.recipes.map(recipe => ({
-      //   id: recipe.id,
-      //   dishName: recipe.title,
-      //   vegetarian: recipe.vegetarian,
-      //   glutenFree: recipe.glutenFree,
-      //   dairyFree: recipe.dairyFree,
-      //   cookingTime: recipe.readyInMinutes,
-      //   servings: recipe.servings,
-      //   image: recipe.image || 'https://images.unsplash.com/photo-1633878353697-f751870d1d76?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
-      //   instruction: Object.assign({}, recipe.analyzedInstructions[0]?.steps.map(s => s.step) || ['Instructions are currently unavailable, please check the full recipe below for more details.']),
-      //   ingredient: Object.assign({}, recipe.extendedIngredients?.map(i => i.original) || ['Ingredients are currently unavailable, please check the full recipe below for more details.']),
-      //   fullDetailsUrl: recipe.spoonacularSourceUrl || '/menu'
-      // })).slice(offset, offset + limit)
-
-      // res.render('menu', {
-      //   recipesData,
-      //   path: `/menu/${preference}`,
-      //   preference,
-      //   limit,
-      //   pagination: getPagination(limit, page, DEFAULT_AMOUNT)
-      // })
     } catch (err) {
       req.flash('warning_msg', 'Could not find the menu!')
       res.render('menu')
