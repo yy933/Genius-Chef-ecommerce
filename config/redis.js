5229e5fdd9d4c3f8ae477dc1b167bb675cac608c
yy933 <emilyyu025@gmail.com>
2023-08-23 16:19:12 +0800

    style: restyle code

--- config/redis.js
+++ config/redis.js
@@ -1,10 +1,7 @@
 require('dotenv').config()
 const Redis = require('ioredis')
-// let redis = new Redis() // Connect to 127.0.0.1:6379
-// if (process.env.NODE_ENV === 'production') {
-//   redis = new Redis(process.env.REDIS_DB_URI)
-// }
 const redis = new Redis(process.env.REDIS_DB_URI)
+
 redis.on('error', (error) => {
   console.log('Redis client error: ', error)
 })
