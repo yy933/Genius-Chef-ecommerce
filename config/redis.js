1edf1e21776c7ed33713b4191272e16d24586a26
yy933 <emilyyu025@gmail.com>
2023-10-13 16:21:15 +0800

    chore: update redis config

--- config/redis.js
+++ config/redis.js
@@ -1,10 +1,6 @@
 require('dotenv').config()
 const Redis = require('ioredis')
-// development
-const redis = new Redis() // connect to 127.0.0.1:6379
-
-// production
-// const redis = new Redis(process.env.REDIS_DB_URI)
+const redis = new Redis(process.env.REDIS_DB_URI)
 
 redis.on('error', (error) => {
   console.log('Redis client error: ', error)
